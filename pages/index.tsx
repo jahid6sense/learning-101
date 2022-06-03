f7c2f4da51f03408b5166f9ddfa15be7f04e6590
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-06-03 14:59:43 +0600

    removing cnsl lg by babelrc

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
 
-  console.log('in local');
+  console.log('in local log');
+  console.error('in local error');
+  console.warn('in local warn');
+  console.info('in local info');
+  console.debug('in local debug');
 
   return (
     <div>
