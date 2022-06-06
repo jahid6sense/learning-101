9a39ec87de3f89cf770365c19c1ac78cfe228f23
jahid6sense <jahid@6sensehq.com>
2022-06-06 17:46:28 +0600

    eslint and some config and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import type { NextPage } from "next";
 import Top from "@Components/Top";
 
 const Home: NextPage = () => {
-    const i = 10;
-    console.log(i);
-    console.log("in local log");
-    console.error("in local error");
-    console.warn("in local warn");
-    console.info("in local info");
-    console.debug("in local debug");
 
-    return (
-        <div>
-            <Top></Top>
-        </div>
-    );
+  const i = 10;
+  console.log(i);
+  console.log("in local log");
+  console.error("in local error");
+  console.warn("in local warn");
+  console.info("in local info");
+  console.debug("in local debug");
+
+  return (
+    <div>
+      <Top></Top>
+    </div>
+  );
 };
 
 export default Home;
