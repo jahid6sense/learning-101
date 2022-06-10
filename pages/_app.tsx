9503008d66fe4ae50183cb77271ad4372634cf22
Robustdevs5 <jahidhasananik.official@gmail.com>
2022-06-10 21:25:03 +0600

    redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-// @ts-ignore
 import * as Sentry from '@sentry/nextjs';
 import { Provider } from "react-redux";
 import store from "redux/store";
