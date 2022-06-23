c2bd3d4b85f6e1c7e956808b7d7f58761b8c69aa
jahid6sense <jahid@6sensehq.com>
2022-06-23 14:06:58 +0600

    google map circle and markers

--- Components/GoogleMap/index.tsx
+++ Components/GoogleMap/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import MyMapComponent from "@Components/MyMapComponent";
 
 
 const index = () => {
+
+  const [latitude, setLatitude] = useState(23.760397389753912);
+  const [longitude, setLongitude] = useState(90.43607491287335);
+
+  const [myPosition, setMyPosition] = useState({ lat: latitude, lng: longitude })
+
+  const generateHouses = (position = myPosition) => {
+    const houses = [];
+    for (let i = 0; i < 100; i++) {
+      const direction = Math.random() < 0.5 ? -50 : 50;
+      houses.push({
+        lat: position.lat + Math.random() / direction,
+        lng: position.lng + Math.random() / direction,
+      });
+    }
+    return houses;
+  };
+
+  const houseArr = useMemo(() => generateHouses(myPosition), [myPosition]);
+
+  const myPositionIcon = <img src="/images/3mb" />;
+
   return (
     <div>
       <h1>Google Map</h1>
@@ -12,6 +34,11 @@ const index = () => {
 
       <MyMapComponent
         isMarkerShown
+        latitude={latitude}
+        longitude={longitude}
+        myPosition={myPosition}
+        houseArr={houseArr}
+        myPositionIcon={myPositionIcon}
         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.googleMapsKey}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
--- Components/MyMapComponent/index.jsx
+++ Components/MyMapComponent/index.jsx
@@ -3,14 +3,58 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  Circle,
+  MarkerClusterer,
 } from "react-google-maps";
+
+const defaultOptions = {
+  strokeOpacity: 0.5,
+  strokeWeight: 2,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+};
+const closeOptions = {
+  ...defaultOptions,
+  zIndex: 3,
+  fillOpacity: 0.05,
+  strokeColor: "#8BC34A",
+  fillColor: "#8BC34A",
+};
+const middleOptions = {
+  ...defaultOptions,
+  zIndex: 2,
+  fillOpacity: 0.05,
+  strokeColor: "#FBC02D",
+  fillColor: "#FBC02D",
+};
+const farOptions = {
+  ...defaultOptions,
+  zIndex: 1,
+  fillOpacity: 0.05,
+  strokeColor: "#FF5252",
+  fillColor: "#FF5252",
+};
 
 const MyMapComponent = withScriptjs(
   withGoogleMap((props) => (
-    <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
+    <GoogleMap defaultZoom={14} defaultCenter={props.myPosition}>
       {props.isMarkerShown && (
-        <Marker position={{lat: -34.397, lng: 150.644}} />
+        <Marker icon={<img src="/images/3mb.jpg" />} position={props.myPosition} />
       )}
+
+      <Circle center={props.myPosition} radius={1000} options={closeOptions} />
+      <Circle center={props.myPosition} radius={2000} options={middleOptions} />
+      <Circle center={props.myPosition} radius={3000} options={farOptions} />
+
+      {/* <MarkerClusterer>
+        {(clusterer) => */}
+      {props?.houseArr.map((house) => (
+        <Marker key={house.lat} position={house} onClick={() => {}} />
+      ))}
+      {/* }
+      </MarkerClusterer> */}
     </GoogleMap>
   ))
 );
